5b631fabf5ee860c85bbf7f6a5c9c48eab42623a
MUProgrammer <muprogrammer295@gmail.com>
2026-08-24 10:40:37 -0800

    create updateCurrentUserProfile routes and controller in backend

--- backend/controllers/auth_controllers/updateProfile.controller.js
+++ backend/controllers/auth_controllers/updateProfile.controller.js
@@ -1,8 +1,11 @@
+import User from "../../models/user_models/user.models";
 
 
 const updateProfile = async (req, res) => {
     try {
-        
+        // fetch user from database
+        const user = await User.findById(req.user._id);
+
     } catch (error) {
         
     }
