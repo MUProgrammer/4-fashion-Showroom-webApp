eca77454a578e06139b8a7b60fea5d6fc4c01559
MUProgrammer <muprogrammer295@gmail.com>
2026-09-05 11:44:48 -0800

    update the article model

--- backend/models/article_models/article.model.js
+++ backend/models/article_models/article.model.js
@@ -1,8 +1,20 @@
 import mongoose from "mongoose";
+const colorSchema = new mongoose.Schema({
+  color: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true, default: 0 },
+});             
+const variantSchema = new mongoose.Schema({
+  size: { type: String, required: true },
+  colors: [colorSchema],
+});
+const articalSchema = new mongoose.Schema({
+  articleName: {
+    type: String,
+    required: true,
+  },
+  variants: [variantSchema],
+});
 
-const articleSchema = new mongoose.Schema(
-    {
-        // adticle schema model
-    },
-    { timestamps: true }
-);
+const Article = mongoose.model("Article", articalSchema);
+export default Article;
