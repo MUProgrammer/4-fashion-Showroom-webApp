4a950a74b3048ae18c66adeef83bffc131b199de
MUProgrammer <muprogrammer295@gmail.com>
2026-09-14 22:47:07 -0800

    update the article service

--- backend/controllers/services/article.service.js
+++ backend/controllers/services/article.service.js
@@ -2,4 +2,11 @@ import express from "express";
 import Article from "../../models/article_models/article.models.js";
 
 
-// 
+const getAllArticles = async (req, res) => {
+    try {
+        const articles = await Article.find();
+        res.status(200).json(articles);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
