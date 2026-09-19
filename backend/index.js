fa792630da0f83c44284f5011448d36eb45e68d2
MUProgrammer <muprogrammer295@gmail.com>
2026-09-18 23:55:09 -0800

    add new files

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 // connect to the database
 connectDB();
-
+ // 
 // middlewares
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
